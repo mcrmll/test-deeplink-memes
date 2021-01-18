29fb8c5934419144863d00796962327df52a4c0c
Matheus <Ciaramella>
2021-01-18 16:49:03 +0100

    mnemes

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const serverPort = process.env.PORT || 4444
-var fs = require('fs'),
-    http = require('http');
 
-http.createServer(function (req, res) {
-  fs.readFile(__dirname + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
+async function memes(){
+const serverPort = process.env.PORT || 4444
+const httpsLocalhost = require("https-localhost")()
+let fs = require('fs')
+const https = require('https');
+
+const certs = await httpsLocalhost.getCerts()
+
+https.createServer(certs, (req, res) => {
+    fs.readFile(__dirname + req.url, function (err,data) {
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        res.writeHead(200);
+        res.end(data);
+      });
 }).listen(serverPort);
+}
+
+memes()
