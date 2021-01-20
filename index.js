2c16e5e1113ec9a590c89120e805f9d22af791b4
Matheus <Ciaramella>
2021-01-20 09:50:00 +0100

    fix

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-
-async function memes(){
 const serverPort = process.env.PORT || 4444
-const httpsLocalhost = require("https-localhost")()
 let fs = require('fs')
-const https = require('https');
-
-const certs = await httpsLocalhost.getCerts()
-
-https.createServer(certs, (req, res) => {
+const http = require('http');
+http.createServer((req, res) => {
     fs.readFile(__dirname + req.url, function (err,data) {
         if (err) {
           res.writeHead(404);
@@ -18,6 +12,3 @@ https.createServer(certs, (req, res) => {
         res.end(data);
       });
 }).listen(serverPort);
-}
-
-memes()
