1929803f146e61513f4e066d2475dc2553c2d558
Matheus <Ciaramella>
2021-01-31 17:16:20 +0100

    a

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const serverPort = process.env.PORT || 4444
-let fs = require('fs')
-var mime = require('mime-types')
-const http = require('http');
-http.createServer((req, res) => {
-    fs.readFile(__dirname + req.url, function (err,data) {
-        if (err) {
-          res.writeHead(404);
-          res.end(JSON.stringify(err));
-          return;
-        }
-        if(req.url === "/apple-app-site-association")
-          res.setHeader("Content-Type", "application/pkcs7-mime");
-        res.writeHead(200);
-        res.end(data);
-      });
-}).listen(serverPort);
+const serverPort = process.env.PORT || 4444;
+let fs = require("fs");
+var mime = require("mime-types");
+const http = require("http");
+http
+  .createServer((req, res) => {
+    fs.readFile(__dirname + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      if (req.url === ".well-known/apple-app-site-association")
+        res.setHeader("Content-Type", "application/pkcs7-mime");
+
+      if (req.url === ".well-known/assetlinks")
+        res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end(data);
+    });
+  })
+  .listen(serverPort);
