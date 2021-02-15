7cbac5eba47c26fe72c5df89b4695626517c6897
Matheus <Ciaramella>
2021-02-15 10:53:02 +0100

    adding express

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const serverPort = process.env.PORT || 4444;
-let fs = require("fs");
-var mime = require("mime-types");
-const http = require("http");
-http
-  .createServer((req, res) => {
-    fs.readFile(__dirname + req.url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end(JSON.stringify(err));
-        return;
-      }
-      if (req.url === "/.well-known/apple-app-site-association")
-        res.setHeader("Content-Type", "application/pkcs7-mime");
+var baseDirectory = __dirname   // or whatever base directory you want
+var express = require("express");
+var app = express();
 
-      if (req.url === "/.well-known/assetlinks.json")
-        res.setHeader("Content-Type", "application/json");
-      res.writeHead(200);
-      res.end(data);
-    });
-  })
-  .listen(serverPort);
+var app = express();
+app.use(express.static(baseDirectory)); //use static files in ROOT/public folder
+
+app.get("/", function(request, response){ //root dir
+    response.send("Hello!!");
+});
+
+app.get("/chat", function(request, response){ //root dir
+  response.sendFile('chat.html', {root: __dirname })
+});
+
+app.listen(serverPort, 'localhost');
