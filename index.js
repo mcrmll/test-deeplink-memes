078a0f4729da10174a5cf594b46f9f3f32cacac0
Matheus <Ciaramella>
2021-02-15 10:56:30 +0100

    adding express

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ var app = express();
 var app = express();
 app.use(express.static(baseDirectory)); //use static files in ROOT/public folder
 
-app.get("/", function(request, response){ //root dir
-    response.send("Hello!!");
-});
-
 app.get("/chat", function(request, response){ //root dir
   response.sendFile('chat.html', {root: __dirname })
 });
